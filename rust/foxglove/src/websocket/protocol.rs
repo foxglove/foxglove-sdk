800018af1b4281777f1c33a96d45b46f66818e57
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-03-13 16:58:54 -0700

    yikes

--- rust/foxglove/src/websocket/protocol.rs
+++ rust/foxglove/src/websocket/protocol.rs
@@ -1,2 +1,9 @@
 pub mod client;
 pub mod server;
+
+/// Returns true if the given `schema_encoding` is one of the types that is known to require binary
+/// schema data (i.e. `protobuf` and `flatbuffer`). These require base64-encoding/decoding to be
+/// sent via JSON messages on the websocket connection.
+fn is_known_binary_schema_encoding(schema_encoding: &str) -> bool {
+    schema_encoding == "protobuf" || schema_encoding == "flatbuffer"
+}
