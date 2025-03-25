368f1a6b7b73bcfb9def69014569d881c252f393
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-03-25 11:23:20 -0700

    missing include

--- cpp/foxglove/tests/test_mcap.cpp
+++ cpp/foxglove/tests/test_mcap.cpp
@@ -4,9 +4,11 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
+#include <array>
 #include <filesystem>
 #include <fstream>
 #include <optional>
+
 using Catch::Matchers::ContainsSubstring;
 using Catch::Matchers::Equals;
 
