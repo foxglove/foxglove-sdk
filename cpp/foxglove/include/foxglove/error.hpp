9d4043784de0ba57d8ee9c37224d0f7086031287
Bryan Fox <bryan@foxglove.dev>
2025-05-06 15:18:55 -0400

    Update docs

--- cpp/foxglove/include/foxglove/error.hpp
+++ cpp/foxglove/include/foxglove/error.hpp
@@ -24,9 +24,22 @@ enum class FoxgloveError : uint8_t {
   McapError
 };
 
+ /// @brief A result type for Foxglove operations.
+ ///
+ /// This is similar to `Result` from std::expected (C++23).
+ ///
+ /// You can determine if the result is successful by checking `.has_value()`. If the result is
+ /// successful, error will be FoxgloveError::Ok and the expected data can be unwrapped with
+ /// `.value()`. Otherwise, the error type can be extracted with `.error()`.
+ ///
+ /// @tparam T The type of the success value returned by the operation.
 template<typename T>
 using FoxgloveResult = expected<T, FoxgloveError>;
 
+/// @brief A string representation of a FoxgloveError.
+///
+/// @param error The error to convert to a string.
+/// @return A C string representation of the error.
 const char* strerror(FoxgloveError error);
 
 }  // namespace foxglove
