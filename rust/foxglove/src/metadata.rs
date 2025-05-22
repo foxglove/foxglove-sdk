df9ed916af8e1eae48c59c5fbe1b79ffbe3230e3
Eloff <dan.eloff@gmail.com>
2025-05-21 18:48:52 -0600

    rename reference IntoFoo -> ToUnixNanos in doc comments

--- rust/foxglove/src/metadata.rs
+++ rust/foxglove/src/metadata.rs
@@ -5,30 +5,30 @@ use crate::schemas::Timestamp;
 /// A trait for converting a time value to a u64 nanoseconds since epoch.
 pub trait ToUnixNanos {
     /// Returns the value as nanoseconds since epoch.
-    fn to_unix_nanos(self) -> u64;
+    fn to_unix_nanos(&self) -> u64;
 }
 
 impl ToUnixNanos for u64 {
-    fn to_unix_nanos(self) -> u64 {
+    fn to_unix_nanos(&self) -> u64 {
         self
     }
 }
 
 impl ToUnixNanos for Timestamp {
-    fn to_unix_nanos(self) -> u64 {
+    fn to_unix_nanos(&self) -> u64 {
         self.total_nanos()
     }
 }
 
 impl ToUnixNanos for SystemTime {
-    fn to_unix_nanos(self) -> u64 {
+    fn to_unix_nanos(&self) -> u64 {
         self.duration_since(UNIX_EPOCH).unwrap().as_nanos() as u64
     }
 }
 
 #[cfg(feature = "chrono")]
 impl ToUnixNanos for chrono::DateTime<chrono::Utc> {
-    fn to_unix_nanos(self) -> u64 {
+    fn to_unix_nanos(&self) -> u64 {
         self.timestamp_nanos_opt().expect("timestamp out of range") as u64
     }
 }
@@ -45,7 +45,7 @@ impl PartialMetadata {
     /// Returns a new PartialMetadata with the given log time.
     ///
     /// `log_time` can be a u64 (nanoseconds since epoch), a foxglove [`Timestamp`][crate::schemas::Timestamp],
-    /// a [`SystemTime`][std::time::SystemTime], or anything else that implements [`IntoFoo`][crate::IntoFoo].
+    /// a [`SystemTime`][std::time::SystemTime], or anything else that implements [`ToUnixNanos`][crate::ToUnixNanos].
     pub fn with_log_time(log_time: impl ToUnixNanos) -> Self {
         Self {
             log_time: Some(log_time.to_unix_nanos()),
