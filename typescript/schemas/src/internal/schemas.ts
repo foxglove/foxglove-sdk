07d9b24160422745a0cb470b398ab758e8a2d82b
Bryan Fox <bryan@foxglove.dev>
2025-07-29 12:48:34 -0400

    Add color to LocationFix

--- typescript/schemas/src/internal/schemas.ts
+++ typescript/schemas/src/internal/schemas.ts
@@ -1367,24 +1367,30 @@ const LocationFix: FoxgloveMessageSchema = {
         "If `position_covariance` is available, `position_covariance_type` must be set to indicate the type of covariance.",
       protobufFieldNumber: 5,
     },
+    {
+      name: "color",
+      type: { type: "nested", schema: Color },
+      description: "Color used to visualize the location",
+      protobufFieldNumber: 8,
+    },
   ],
 };
 
 const LocationFixes: FoxgloveMessageSchema = {
   type: "message",
   name: "LocationFixes",
-  description: "A series of LocationFix messages",
+  description: "A group of LocationFix messages",
   fields: [
     {
       name: "fixes",
       type: { type: "nested", schema: LocationFix },
       array: true,
-      description: "A series of location fixes",
+      description: "One or more location fixes",
     },
     {
       name: "color",
       type: { type: "nested", schema: Color },
-      description: "Color used to visualize this series",
+      description: "Color used to visualize this series, if not set on the individual fixes",
     },
   ],
 };
