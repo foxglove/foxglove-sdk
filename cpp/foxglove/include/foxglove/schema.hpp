71f58b9c0b3d59f7baca771591753c568470161d
James Smith <james@foxglove.dev>
2025-08-26 11:27:56 +1000

    add schema.hpp

---
+++ cpp/foxglove/include/foxglove/schema.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+namespace foxglove {
+
+/// A Schema is a description of the data format of messages in a channel.
+///
+/// It allows Foxglove to validate messages and provide richer visualizations.
+/// See the [MCAP spec](https://mcap.dev/spec#schema-op0x03) for more information.
+struct FoxgloveSchema {
+  /// An identifier for the schema.
+  std::string_view name;
+  /// The encoding of the schema data. For foxglove schemas, this is always "protobuf".
+  /// [well-known schema encodings]: https://mcap.dev/spec/registry#well-known-schema-encodings
+  std::string_view encoding;
+  /// The schema data.
+  const uint8_t* data;
+  /// The length of the schema data.
+  size_t data_len;
+};
+
+}  // namespace foxglove
