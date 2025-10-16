1fde886c6a79135cecf8a4fb8add7326ef4f6652
Sebastian Urbina <sebastian@foxglove.dev>
2025-10-16 13:09:20 -0300

    fix docs

--- python/foxglove-sdk/python/docs/conf.py
+++ python/foxglove-sdk/python/docs/conf.py
@@ -32,7 +32,7 @@ nitpick_ignore_regex = [
     # autodoc_typehints also fails on Capability which is imported in websocket.py, but is
     # manually documented as an enum
     ("py:class", r"foxglove\.Capability"),
-    ("py:class", r"FoxgloveWidget"),
+    ("py:class", r"DataSource"),
 ]
 
 templates_path = ["_templates"]
